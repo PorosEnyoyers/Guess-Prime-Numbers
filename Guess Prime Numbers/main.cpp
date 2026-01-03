e16a8e9ef5ed431370a1a87ad8209f5bbf650bc8
Poros Enjoyers <138520882+PorosEnyoyers@users.noreply.github.com>
2026-01-02 16:15:19 -0800

    1

---
+++ Guess Prime Numbers/main.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include "Random.h"
+#include <cmath>
+#include <cassert>
+#include <functional>
+#include <string>
+#include <optional>
+#include <format>
+
+bool is_prime(int n)
+{
+	if (n == 2 || n == 3)
+		return true;
+	if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+		return false;
+	
+	for (int i{ 5 }; i * i <= n; ++i)
+	{
+		if (n % i == 0)
+			return false;
+	}
+
+	return true;
+}
+
+int generate_prime_number()
+{
+	std::random_device re;
+	std::mt19937 engine(re());
+	std::uniform_int_distribution<int> dist(1, 99999);
+	int n{};
+	while (!is_prime(n))
+	{
+		n = Random::get(1,99999);
+	}
+	return n;
+}
+
+void ignoreLine()
+{
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int input()
+{
+	int num{};
+	while (!(std::cin >> num))
+	{
+		std::cin.clear();
+		ignoreLine();
+		std::cout << "Invalid inputs!!! Please enter a number: \n";
+	}
+	return num;
+}
+
+void guess_number(int number)
+{
+	std::cout << "Guess the number: \n";
+	int guess = input();
+	while (guess != number)
+	{
+		std::cout << guess << " is wrong. Try again\n";
+		guess = input();
+	}
+	std::cout << "Well done. \n";
+}
+
+std::optional<int> read_number(std::istream& in)
+{
+	int result{};
+	if (in >> result)
+	{
+		return result;
+	}
+	in.clear();
+	in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	return {};
+}
+
+void guess_number_or_give_up(int num)
+{
+	std::cout << "Guess the number or Enter any letter to quit the game: \n";
+	std::optional<int> guess;
+	while ((guess = read_number(std::cin)))
+	{
+		if (guess.value() == num)
+		{
+			std::cout << "Well done!";
+			return;
+		}
+		std::cout << guess.value() << " is wrong. Try again.\n";
+	}
+	std::cout << "Exit the program. The number was " << num << '\n';
+}
+
+void guess_number_with_clues(int number, auto message)
+{
+	std::cout << "Guess the number or Enter any letter to quit. \n";
+	std::optional <int> guess;
+	while ((guess = read_number(std::cin)))
+	{
+		if (guess.value() == number)
+		{
+			std::cout << "Well done.";
+			return;
+		}
+		std::cout << std::format("{:0>5}", (guess.value())) << " is wrong. Try again\n";
+		std::cout << message(number, guess.value());
+	}
+	std::cout << std::format("Exit the program!!! The number was {:0>5}\n", (number));
+}
+
+std::string check_which_digits_correct(int number, int guess)
+{
+	auto ns = std::format("{:0>5}", (number));
+	auto gs = std::format("{:0>5}", (guess));
+	std::string matches(5, '.');
+	for (size_t i{ 0 }, stop{ gs.length() }; i < stop; ++i)
+	{
+		char guess_char = gs[i];
+		if (guess_char == ns[i])
+		{
+			matches[i] = '*';
+			ns[i] = '*';
+		}
+	}
+	for (size_t i{ 0 }, stop{ gs.length() }; i < stop; ++i)
+	{
+		char guess_char = gs[i];
+		if (matches[i] != '*')
+		{
+			if (size_t idx{ ns.find(guess_char, 0) }; idx != std::string::npos)
+			{
+				matches[i] = '^';
+				ns[idx] = '^';
+			}
+		}
+	}
+	return matches;
+}
+
+int main()
+{
+	auto message = [](int number, int guess) {
+		return std::format("{}\n", check_which_digits_correct(number, guess));
+		};
+	guess_number_with_clues(generate_prime_number(), message);
+	return 0;
+}
